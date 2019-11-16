eb300b8bb4911f0f2ff42d73fdc89e577d1f24a4
dzmsrg <Dzmitrock87@gmail.com>
2019-11-16 12:13:17 +0300

    Basket with add and delete options

--- dzm_site/orders/views.py
+++ dzm_site/orders/views.py
@@ -12,11 +12,22 @@ def basket_adding(request):
     data = request.POST
     product_id = data.get('prod_id')
     nmb = data.get('number')
+    is_delete = data.get('is_delete')
 
-    new_product, created = ProductInBasket.objects.get_or_create(session_key=session_key, product_id=product_id, defaults={'count':nmb})
-    if not created:
-        new_product.count +=int(nmb)
-        new_product.save(force_update=True)
+    if is_delete=='true':
+        # если находимся в этом блоке кода то:
+        # product_id = id корзины
+        ProductInBasket.objects.filter(id=product_id).update(count=0, is_active=False)
+    else:
+        # если находимся в этом блоке кода то:
+        # product_id = id продукта
+        new_product, created = ProductInBasket.objects.get_or_create(session_key=session_key, product_id=product_id, defaults={'count':nmb})
+        if not created:
+            if ProductInBasket.objects.filter(product_id=product_id, is_active=False):
+                new_product.is_active=True
+            new_product.count +=int(nmb)
+            new_product.save(force_update=True)
+
     prods_in_basket = ProductInBasket.objects.filter(session_key=session_key, is_active=True)
     total_count_in_basket = 0
     for item_in_basket in prods_in_basket:
@@ -25,9 +36,20 @@ def basket_adding(request):
     products_in_basket_count = total_count_in_basket
 
     return_dict['products_in_basket_count'] = products_in_basket_count
-    return_dict['was_created'] = created
-    return_dict['prod_id_count'] = new_product.count
+
+
+    return_dict["products"] = list()
+    for item in prods_in_basket:
+        product_dict = dict()
+        product_dict["id"] = item.id
+        product_dict["name"] = item.product.name
+        product_dict["price_per_item"] = item.price_per_item
+        product_dict["nmb"] = item.count
+        return_dict["products"].append(product_dict)
+
     print(f'return_dict: {return_dict}')
+            
+
 
     return JsonResponse(return_dict)
 
--- dzm_site/static/static_dev/js/scripts.js
+++ dzm_site/static/static_dev/js/scripts.js
@@ -1,25 +1,23 @@
 $ (document).ready(function(){
     var form = $('#form_buying_product');
     console.log(form);
-    form.on('submit', function(e){
-        e.preventDefault();
-        var nmb = $('#number').val();
-        console.log(nmb);
-        var submit_btn = $('#submit_btn');
-        var product_id = submit_btn.data('product_id');
-        var product_name = submit_btn.data('product_name');
-        var product_price = submit_btn.data('product_price')
-        console.log(product_id);
-        console.log(product_name);
-        console.log(product_price);
 
+
+
+    function basket_updating(product_id, nmb, is_delete=false){
         var data_prod = {}
-        data_prod["prod_id"] = product_id; // синтаксис добавления в словарь или через точку или через []
+        data_prod["prod_id"] = product_id; // это может быть id продукта или id корзины (все обусловлено тем, кто вызвал событие)
+        // ключевым разграничением будет отправлять параметр is_delete=false для "купить" и is_delete=true для "удалить"000000
         data_prod.number = nmb;
         console.log(data_prod);
         var csrf_token = $('#form_buying_product [name="csrfmiddlewaretoken"]').val();
         data_prod["csrfmiddlewaretoken"] = csrf_token;
         console.log(data_prod);
+
+        if(is_delete){
+            data_prod['is_delete']=true
+        }
+
         var form_url = form.attr("action");
         console.log(form_url)
 
@@ -32,24 +30,37 @@ $ (document).ready(function(){
                 console.log("OK")
                 console.log(data_response.products_in_basket_count)
                 if (data_response.products_in_basket_count){
-                    $('#basket_total_count').text("("+data_response.products_in_basket_count+")")
-                    console.log(data_prod)
-                    console.log(data_response)
+                    $('#basket_total_count').text("("+data_response.products_in_basket_count+")");
+                    console.log(data_prod);
+                    console.log(data_response);
 
 
-                    if (data_response.was_created){
-                        $('.basket-item ul').append('<li id="'+product_id+'">' +
-                        product_name + ' x ' + nmb +
-                        ' шт. '+ product_price+'   руб.'+
-                //        '<a class="delete-item" href="">x</a>'+
-                        ' </li>')
-                    } else {
-                        console.log('in else')
-                        $('#'+product_id).text(product_name + ' x ' + data_response.prod_id_count +
-                        ' шт. '+ product_price+'   руб.')
+                     $('.basket-item ul').html(""); // очищаем корзину
+                     $.each(data_response.products, function(k, v){   // проходимся по каждому элементу массива
+                        console.log('ind: '+ k +" "+ 'val.name:'+ v.name);
+                        $('.basket-item ul').append('<li>'+ v.name+', ' + v.nmb + 'шт. ' + 'по ' + v.price_per_item + 'руб. ' +
+                            '<a class="delete-item" href="" data-product_id="'+v.id+'">x</a>'+
+                            '</li>');
+                     });
 
-                    }
 
+//                    if (data_response.was_created){
+//                        $('.basket-item ul').append('<li id="'+product_id+'">' +
+//                        product_name + ' x ' + nmb +
+//                        ' шт. '+ product_price+'   руб.'+
+//                        '<a class="delete-item" href="" data-product_id="'+product_id+
+//                        ' </li>')
+//
+//
+//                    } else {
+//                        console.log('in else')
+//                        $('#'+product_id).text(product_name + ' x ' + data_response.prod_id_count +
+//                        ' шт. '+ product_price+'   руб.')
+//                        var prod_el = document.getElementById(product_id);
+//                        //добавляем в html крестик - удалить элемент
+//                        prod_el.insertAdjacentHTML('beforeEnd', '<a class="delete-item" href="" data-product_id="'+
+//                            product_id+'>x</a>');
+//                    }
                 }
             },
 
@@ -57,17 +68,29 @@ $ (document).ready(function(){
                 console.log("error")
 
             }
-
         })
+    }
 
 
+    form.on('submit', function(e){
+        e.preventDefault();
+        var nmb = $('#number').val(); //считываем значение с поля ввода input c id="number" в product.html
+        console.log(nmb);
+        var submit_btn = $('#submit_btn'); // Кнопка КУПИТЬ с id="submit_btn" в product.html
+        var product_id = submit_btn.data('product_id'); // считываем аттрибуты data-product_id с кнопки КУПИТЬ
+//        var product_name = submit_btn.data('product_name'); //data-product_name
+//        var product_price = submit_btn.data('product_price'); // data-product_price
+//        console.log(product_id);
+//        console.log(product_name);
+//        console.log(product_price);
+        basket_updating(product_id, nmb, is_delete=false)
     });
 
     function showingBasket() {
         $('.basket-item').toggleClass('hidden');
 //        $('.basket-item').removeClass('hidden');
-
     }
+
 
     $('.basket-container').on('click', function(e){
         e.preventDefault();
@@ -88,11 +111,13 @@ $ (document).ready(function(){
 
     $(document).on('click', '.delete-item', function(e){
         e.preventDefault();
-        $(this).closest('li').remove();
+        product_id=$(this).data("product_id");
+        basket_updating(product_id, nmb=0, is_delete=true);
+
+
+//        $(this).closest('li').remove();
 
     })
-
-
 
 
 })
